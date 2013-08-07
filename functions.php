ec5d68cdadf83ffeb5ad8c113cd384a1d17c6797
TilekDzh <tilek-jamankulov@mail.ru>
2013-08-07 20:01:57 +0600

    eee

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 	// TESTING COMMENT SECTION
 	function getConnection() {
 
-		$connection = mysqli_connect("mysql6.000webhost.com", "a7551670_auca" , "rootAUCA2014", "a7551670_auca");		
+		$connection = mysqli_connect("mysql6.000webhost.com", "a7551670_auca" , "rootAUCA2014", "a7551670_auca");		);		
 
 		return $connection;	
 	}
@@ -197,6 +197,7 @@
 			$query = "SELECT notificationID,notification_status FROM usernotif WHERE userID = '$userID'";
 			$info = mysqli_query($connection,$query);
 			
+			$notificationInfo = array();
 			while($row =  mysqli_fetch_array($info))
 			{
 				$notificationID = $row['notificationID'];
@@ -207,14 +208,12 @@
 				
 				$notificationID = 1;
 				$ID = (string)$notificationID;
-			
-				$notificationInfo = array();
 				
 				$notificationInfo[$ID] = $result;
 				
-				echo json_encode($notificationInfo);
-				
 			}
+			
+			echo json_encode($notificationInfo);
 			//Notification processes here
 			//$_GET['user_id']
 			
